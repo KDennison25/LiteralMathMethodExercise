45ea34ede5634918718ab477f2e72a5091902ca2
Student <student@BCCSs-MacBook-Pro.local>
2024-02-01 10:53:22 -0600

    Exercise completed

---
+++ script.js
@@ -0,0 +1,14 @@
+ let warmHugs = "Hi, I'm Olaf and I like warm hugs." // 1a
+console.log(warmHugs.toUpperCase()); // 1b
+console.log(warmHugs.replace('like', 'love')); // 1c
+warmHugs = warmHugs.replace('like','love')
+let beenImpaled = "Oh, look at that. I've been impaled." // 2a
+console.log(beenImpaled.slice(17)); // 2b
+let dotDotDot = "..." // 3a
+let skullBones = `I don't have a skull${dotDotDot}or bones.` // 3b
+console.log(skullBones);
+console.log(Math.PI); // 4
+let randomNumber = Math.random() * 3
+// console.log(randomNumber);
+randomNumber = Math.ceil(randomNumber)
+console.log(randomNumber); // 5
